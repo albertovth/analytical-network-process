7d897c2182b358ae6846d2bdb3395eda51148d81
Alberto Valiente Thoresen <46265925+albertovth@users.noreply.github.com>
2024-03-06 19:22:48 +0100

    Update analytical-network-process-AVT.py

--- analytical-network-process-AVT.py
+++ analytical-network-process-AVT.py
@@ -103,7 +103,7 @@ def draw_network_diagram(diagram_title, main_criteria_node_list, criteria_node_l
     for index, node in enumerate(alternative_node_list):
         pos[node] = np.array([index * alternative_spacing, alternative_heights[index % len(alternative_heights)]])
 
-    fig, ax = plt.subplots(figsize=(10,6))
+    fig, ax = plt.subplots(figsize=(18,12))
     
     node_size_main = 2000
     node_size_network = 1800
